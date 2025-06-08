43a2a38e2040e8936aaee3aa660a9a9264f99143
m9stafa05 <m9stafa05@gmail.com>
2025-06-09 01:31:13 +0300

    Add ProductGridView widget to display products in a grid layout

---
+++ lib/widgets/product_grid_view.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:open_fashion/models/product_model.dart';
+import 'package:open_fashion/widgets/product_card.dart';
+
+class ProductGridView extends StatelessWidget {
+  const ProductGridView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 0.6,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 40,
+      ),
+      itemCount: ProductModel.productsList.length,
+      itemBuilder: (context, index) {
+        final item = ProductModel.productsList[index];
+        return ProductCard(item: item);
+      },
+    );
+  }
+}
