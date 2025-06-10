9004be8c024397e0f02f34e403cc7334b09cf75f
m9stafa05 <m9stafa05@gmail.com>
2025-06-10 07:45:37 +0300

    Refactor AddAddressScreenBody to  implement form validation with saved address handling

--- lib/widgets/add_address_screen_body.dart
+++ lib/widgets/add_address_screen_body.dart
@@ -3,20 +3,34 @@ import 'package:open_fashion/widgets/add_address_form.dart';
 import 'package:open_fashion/widgets/custom_bottom.dart';
 import 'package:open_fashion/widgets/custom_text_header.dart';
 
-class AddAddressScreenBody extends StatelessWidget {
+class AddAddressScreenBody extends StatefulWidget {
   const AddAddressScreenBody({super.key});
 
   @override
+  State<AddAddressScreenBody> createState() =>
+      _AddAddressScreenBodyState();
+}
+
+class _AddAddressScreenBodyState extends State<AddAddressScreenBody> {
+  final _formKey = GlobalKey<AddAddressFormState>();
+
+  @override
   Widget build(BuildContext context) {
+    final savedAddress =
+        ModalRoute.of(context)?.settings.arguments
+            as Map<String, dynamic>?;
     return Column(
       children: [
         CustomTextHeader(text: 'Add shipping address'),
-        AddAddressForm(),
+        AddAddressForm(key: _formKey, savedAddress: savedAddress),
         Spacer(),
         CustomBottom(
           text: 'Add now',
           onTap: () {
-            Navigator.pop(context);
+            if (_formKey.currentState!.validate()) {
+              final formData = _formKey.currentState?.getFormData();
+              Navigator.pop(context, formData);
+            }
           },
           withIcon: false,
         ),
