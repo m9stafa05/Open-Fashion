fac9702262aca7539a03fb9a0981a0963dab4fa5
m9stafa05 <m9stafa05@gmail.com>
2025-06-10 05:57:32 +0300

    Add AddAddressScreen implementation with custom app bar and body

---
+++ lib/screens/add_address_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:open_fashion/widgets/add_address_screen_body.dart';
+import 'package:open_fashion/widgets/custom_app_bar.dart';
+
+class AddAddressScreen extends StatelessWidget {
+  const AddAddressScreen({super.key});
+  static const String id = '/addAddressScreen';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(isBlack: false),
+      body: AddAddressScreenBody(),
+    );
+  }
+}
