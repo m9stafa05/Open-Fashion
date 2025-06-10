cf065efde8e3d9723b7bfba1280f3ed370c296fb
m9stafa05 <m9stafa05@gmail.com>
2025-06-10 04:40:15 +0300

    Add PaymentMethodSelection widget

---
+++ lib/widgets/payment_method_selection.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:open_fashion/widgets/custom_text.dart';
+import 'package:open_fashion/widgets/select_container.dart';
+
+class PaymentMethodSelection extends StatelessWidget {
+  const PaymentMethodSelection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomText(
+            text: 'Payment method'.toUpperCase(),
+            color: Color(0xff888888),
+            size: 14,
+          ),
+          const Gap(8),
+          SelectContainer(
+            text: 'select payment method',
+            icon: Icons.keyboard_arrow_down_sharp,
+            withDefaultSelection: false,
+          ),
+        ],
+      ),
+    );
+  }
+}
