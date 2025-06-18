05a839dff9a74454dc598f08e7c6e472750918f4
m9stafa05 <m9stafa05@gmail.com>
2025-06-18 08:52:55 +0300

    Fix icon color in AddressDataFlow widget for better visibility

--- lib/widgets/address_data_flow.dart
+++ lib/widgets/address_data_flow.dart
@@ -45,7 +45,7 @@ class AddressDataFlow extends StatelessWidget {
             ],
           ),
 
-          Icon(Icons.arrow_forward_ios_outlined),
+          Icon(Icons.arrow_forward_ios_outlined, color: Colors.grey),
         ],
       ),
     );
