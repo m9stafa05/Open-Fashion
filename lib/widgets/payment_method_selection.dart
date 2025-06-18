1b2cf5750a2097ca9f0ea8f7767f665981397f9a
m9stafa05 <m9stafa05@gmail.com>
2025-06-18 06:13:14 +0300

    Add navigation to AddCreditCardScreen in PaymentMethodSelection widget

--- lib/widgets/payment_method_selection.dart
+++ lib/widgets/payment_method_selection.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:open_fashion/screens/add_credit_card_screen.dart';
 import 'package:open_fashion/widgets/custom_text.dart';
 import 'package:open_fashion/widgets/select_container.dart';
 
@@ -20,6 +21,9 @@ class PaymentMethodSelection extends StatelessWidget {
           ),
           const Gap(8),
           SelectContainer(
+            onTap: () {
+              Navigator.pushNamed(context, AddCreditCardScreen.id);
+            },
             text: 'select payment method',
             icon: Icons.keyboard_arrow_down_sharp,
             withDefaultSelection: false,
