e56df4f3b9cdfb7f854c4887a9cf3afe731d38a3
m9stafa05 <m9stafa05@gmail.com>
2025-06-22 13:46:40 +0300

    Add displayed dialog on checkout button tap in PlaceOrderScreenBody

--- lib/widgets/place_order_screen_body.dart
+++ lib/widgets/place_order_screen_body.dart
@@ -4,6 +4,7 @@ import 'package:open_fashion/widgets/checkout_card.dart';
 import 'package:open_fashion/widgets/custom_bottom.dart';
 import 'package:open_fashion/widgets/custom_text_header.dart';
 import 'package:open_fashion/widgets/address_selection.dart';
+import 'package:open_fashion/widgets/displayed_dialog.dart';
 import 'package:open_fashion/widgets/payment_method_selection.dart';
 import 'package:open_fashion/widgets/shopping_method_selection.dart';
 import 'package:open_fashion/widgets/total_cost.dart';
@@ -90,7 +91,18 @@ class _PlaceOrderScreenBodyState extends State<PlaceOrderScreenBody> {
         Gap(10),
         savedCard == null || savedAddress == null
             ? CustomBottom(text: 'Place Order', onTap: () {})
-            : CustomBottom(text: 'Checkout', onTap: () {}),
+            : CustomBottom(
+                text: 'Checkout',
+                onTap: () {
+                  showDialog(
+                    barrierDismissible: false,
+                    context: context,
+                    builder: (BuildContext context) {
+                      return const DisplayedDialog();
+                    },
+                  );
+                },
+              ),
       ],
     );
   }
